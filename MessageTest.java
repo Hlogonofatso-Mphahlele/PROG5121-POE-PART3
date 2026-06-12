1a875ed50ca2a89efda8a5b72284f282075c794b
Hlogonofatso-Mphahlele <ST10519166@rcconnect.edu.za>
2026-06-12 22:35:19 +0200

    pasted the final draft
    removed unnecessary imports and updated it as well

--- MessageTest.java
+++ MessageTest.java
@@ -1,24 +1,22 @@
 
 package chatapp;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class MessageTest {
     
+    //tests to check valild and invalid recipient cellphonr number formats
     @Test
     public void testValidRecipientCellFormat() {
         Message ms = new Message("+27718693002", "Hi Mike, can you join us for dinner tonight?");
-        assertEquals("Cell phone successfully captured", ms.checkRecipientCell());
+        assertEquals("Cell phone number successfully captured.", ms.checkRecipientCell());
     }
     @Test
     public void testInvalidRecipientCellFormat(){
         Message ms = new Message("08575975889", "Hi Keegan, did you recieve the payment?");
-        assertEquals("Cell phone number incorrectly formatted or doesnt contain the international code. Correct the number and try again", ms.checkRecipientCell());
+        assertEquals("Cell phone number incorrectly formatted or doesn't contain the international code. Correct the number and try again.", ms.checkRecipientCell());
     }
     
     //tets for valid and invlid message lengths limits
@@ -33,8 +31,9 @@ public class MessageTest {
 "You can easily change the formatting of selected text in the document text by choosing a look for the selected text from the Quick Styles gallery on the Home tab. You can also format text directly by using the other controls on the Home tab. Most controls offer a choice of using the look from the current theme or using a format that you specify directly.\n" +
 "To change the overall look of your document, choose new Theme elements on the Page Layout tab. To change the looks available in the Quick Style gallery, use the Change Current Quick Style Set command. Both the Themes gallery and the Quick Styles gallery provide reset commands so that you can always restore the look of your document to the original contained in your current template.");
         assertEquals("Message exceeds 250 charecters by 796 charecters. Please reduce the size", ms.validateMessageLength());
-    }
-    
+    }       
+   
+  
     // Message sending options
     @Test
     public void testMessageSendingOption() {
@@ -53,4 +52,23 @@ public class MessageTest {
     }
     
     
+     @Test
+    public void testCreatedMessageID(){
+        Message ms  = new Message("+27186930029", "Hi Keegan, did you recieve the payment?");
+        assertTrue(ms.checkMessageID());
+    }
+    
+    @Test
+    public void testValidMEssageHashFormat() {
+        Message ms = new Message("+27186930029" ,"Hi Mike, can you join us for dinner tonight?");
+        String msHash = ms.getMessageHash();
+        assertTrue(msHash.contains("HITONIGHT"));
+    }
+    
+    @Test
+    public void testReturnTotalNumberOfSendMessages() {
+        Message ms = new Message("+27286930029", "Hey Keegan, did you recieve the payment?");
+        ms.sendMessages(1);
+        assertTrue(Message.returnTotalNumberOfMessages() > 0);
+    }
 }
